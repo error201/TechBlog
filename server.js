6995d6a72aac4f0c2d9adf822d9239db6b04b8cb
Jason A. Arter <jason.arter@gmail.com>
2023-02-09 18:52:35 -0800

    Initial commit.

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const allRoutes = require('./controllers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+// Requiring our models for syncing
+const { User,Chirp} = require('./models');
+
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        maxAge:1000*60*60*1
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+// // Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static('public'));
+
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(allRoutes);
+app.get("/sessions",(req,res)=>{
+    res.json(req.session)
+});
+
+sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+    console.log('App listening on PORT ' + PORT);
+    });
+});
